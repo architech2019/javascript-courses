288097c56214d6494ba8ffdf09a2cac1d7e2711b
Chadrack KANZA <chadrackkanza2015@gmail.com>
2022-10-14 21:12:15 +0100

    lesson 2 : variables

---
+++ variables.js
@@ -0,0 +1,17 @@
+var nom;
+let age;
+const sexe = "m";
+
+age = 5
+nom = "Test"
+console.log(nom)
+console.log(age)
+console.log(sexe)
+
+
+age = 6
+nom = "Example"
+
+console.log(nom)
+console.log(age)
+console.log(sexe)
