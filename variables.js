bbe79190faf974279b5768714e5a7b232f34cae8
Chadrack KANZA <chadrackkanza2015@gmail.com>
2022-10-14 21:16:02 +0100

    lesson 3 : commentaires

--- variables.js
+++ variables.js
@@ -2,13 +2,21 @@ var nom;
 let age;
 const sexe = "m";
 
+// Premier test
 age = 5
 nom = "Test"
 console.log(nom)
 console.log(age)
 console.log(sexe)
 
+/* 
+deuxieme test
 
+Ici c'est un commentaire
+multilignes
+
+
+*/
 age = 6
 nom = "Example"
 
