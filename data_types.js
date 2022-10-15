e069ed1fd26e88cda88674b370b25259534ac31b
Chadrack KANZA <chadrackkanza2015@gmail.com>
2022-10-15 22:58:01 +0100

    lesson 5 : data types, lesson 6 : methods

---
+++ data_types.js
@@ -0,0 +1,19 @@
+// entier
+let x = 5
+
+//String ou chaine de caracteres
+let text = 'anniv '
+document.write(text.substring(0,2))
+
+//Char
+x = 'a'
+
+// Booleen
+let a = true
+let b = false
+
+// null
+let c = null
+
+//undefined
+let z;
---
+++ methods.js
@@ -0,0 +1,21 @@
+function greeting(){
+    document.write("Bonjour")
+}
+
+function somme(a,b){
+    
+    return a + b
+}
+
+function multiple(c , a){
+    if(c == 5){
+        document.write(" c 5")
+    }
+}
+
+
+// Call functions
+
+let c = somme(31, 6)
+
+multiple(5,4)
